5450d1658fb29120d7de2fa8d5525234a55f6ae8
Dave Sanders <davepaulsanders@gmail.com>
2022-07-11 15:10:42 -0400

    Server up

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});

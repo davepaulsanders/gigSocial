db92054e0df5ef5aaf9f0340209773429ec08e4b
Dave Sanders <davepaulsanders@gmail.com>
2022-07-14 09:50:52 -0400

    React router added and log in and sign up redirect to profile

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,44 @@ import {
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import "./App.css";
 import LandingPage from "./pages/LandingPage.js";
+import Profile from "./pages/Profile";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //link to graphql server
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem("id_token");
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : "",
-//     },
-//   };
-// });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 // setting up apollo client
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <LandingPage />
-      </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }

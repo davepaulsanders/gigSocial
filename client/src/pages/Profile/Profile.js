65a1bd0d1f219a65477a5abb970f04dd5c25008c
Dave Sanders <davepaulsanders@gmail.com>
2022-07-14 15:16:26 -0400

    Adding header component

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { BUTTON, SONG } from "../../styled-components/styled-components";
 import "./Profile.css";
-const CLIENT_ID =
-"Q5kRxC0twZ2MRDhNOFRYuNme6W1LiGmPols-Dddpwup8cnBQ5AB9h-mk_Y11O43M";
-const CLIENT_SECRET =
-"UDZmx2IDkOMuHphTTh6J3YU77NxcZqX2tpdx5bQykIGKWlnIGRp1rngHPSRG_Xy7vFuGPmoUJRszeAfDBT4iFQ";
-const link = `https://api.genius.com/oauth/authorize?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=https://localhost:3000/profile&scope=me&state=statevalue&response_type=code`;
-
+import { Header } from "../../components/Header/Header";
+// const link = `https://api.genius.com/oauth/authorize?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=https://localhost:3000/profile&scope=me&state=statevalue&response_type=code`;
+import Auth from "../../utils/frontEndAuth"
 export const Profile = () => {
+
+  const { username, _id } = Auth.getProfile().data
   return (
     <div>
-      <a href={link}>
-        <BUTTON>Click to connect to genius</BUTTON>
-      </a>
+      <Header />
+      <div className="profile-container">
+        <p className="genius-prompt">
+          Hey <span className="username">{username}</span>, you need to log in or create an account at Genius to use{" "}
+          <span className="gig-social">Gig Social!</span>
+        </p>
+        <a href="">
+          <BUTTON>Connect</BUTTON>
+        </a>
+      </div>
     </div>
   );
 };

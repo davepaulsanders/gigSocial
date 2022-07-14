5b5496f9071a2631fc485f53b99d60a18d49f252
Dave Sanders <davepaulsanders@gmail.com>
2022-07-14 14:32:27 -0400

    Still a cors issue, but login page is accesible

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BUTTON, SONG } from "../../styled-components/styled-components";
 import "./Profile.css";
-const lyrics = require("../../assets/lyrics.png");
-const notes = require("../../assets/notes.png");
-const heart = require("../../assets/heart.png");
-
+const CLIENT_ID =
+"Q5kRxC0twZ2MRDhNOFRYuNme6W1LiGmPols-Dddpwup8cnBQ5AB9h-mk_Y11O43M";
+const CLIENT_SECRET =
+"UDZmx2IDkOMuHphTTh6J3YU77NxcZqX2tpdx5bQykIGKWlnIGRp1rngHPSRG_Xy7vFuGPmoUJRszeAfDBT4iFQ";
 const link = `https://api.genius.com/oauth/authorize?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=https://localhost:3000/profile&scope=me&state=statevalue&response_type=code`;
 
 export const Profile = () => {

a875cf0c0b3a50d5bbaf8bfdff12cb5de0fe7b43
Dave Sanders <davepaulsanders@gmail.com>
2022-07-15 10:00:24 -0400

    Changing code flow order

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+
+export const Header = () => {
+    return (
+        <div>
+            <div className="header-container">
+                <a href="/">
+                <p className="header-title">GIG</p>
+                <p className="header-title">SOCIAL</p>
+                </a>
+            </div>
+        </div>
+    )
+}
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -4,15 +4,23 @@ import "./Profile.css";
 import { useQuery } from "@apollo/client";
 import { GET_LINK } from "../../utils/queries"
 import Auth from "../../utils/frontEndAuth"
-import { Header } from "../../components/"
+import { Header } from "../../components/Header/Header"
+
 export const Profile = () => {
   const {loading, data} = useQuery(GET_LINK)
   const { username } = Auth.getProfile().data
 
+
+  
   const handleClick = (e) => {
     e.preventDefault()
-      const { url } = data.getLink
-      window.location.assign(url)
+    const { url } = data.getLink
+    window.location.assign(url)
+    
+  }
+  
+  if (window.location.href.indexOf('?code=') !== -1) {
+    console.log(window.location.href)
   }
   return (
     <div>
@@ -22,7 +30,7 @@ export const Profile = () => {
           Hey <span className="username">{username}</span>, you need to log in or create an account at Genius to use{" "}
           <span className="gig-social">Gig Social!</span>
         </p>
-          <BUTTON onCLick={handleClick}>Connect</BUTTON>
+          <BUTTON onClick={handleClick}>Connect</BUTTON>
       </div>
     </div>
   );

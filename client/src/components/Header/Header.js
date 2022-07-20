9e43b135952dfd189a4b40d356be1be44d750989
Dave Sanders <davepaulsanders@gmail.com>
2022-07-20 12:27:07 -0400

    Added list items, header, and navigation to html

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,40 +8,55 @@ export const Header = () => {
   const toggleMenu = (e) => {
     e.preventDefault();
     const nav = document.querySelector(".nav-list");
-    if (nav.classList.contains('closed')) {
-        nav.classList.remove("closed");
-        nav.classList.add("open");
-        
+    if (nav.classList.contains("closed")) {
+      nav.classList.remove("closed");
+      nav.classList.add("open");
     } else {
-        nav.classList.remove('open')
-        nav.classList.add('closed')
+      nav.classList.remove("open");
+      nav.classList.add("closed");
     }
   };
 
   return (
-    <div>
+    <header>
       <div className="header-container d-flex justify-content-between align-items-center">
         <a href="/">
           <p className="header-title">GIG</p>
           <p className="header-title">SOCIAL</p>
         </a>
-        <img className="hamburger" onClick={toggleMenu} src={hamburger} alt="" />
-        <nav className="nav-list d-flex align-items-center closed">
-          <a className="nav-link" href="">
-            Browse
-          </a>
-          <a className="nav-link" href="/setlists">
-            Setlists
-          </a>
-          <button
-            onClick={() => Auth.logout()}
-            className="nav-log-out nav-link me-4"
-          >
-            Log Out
-          </button>
-          <img onClick={toggleMenu} className="plus-menu-exit" src={plus} alt="exit" />
+        <img
+          className="hamburger"
+          onClick={toggleMenu}
+          src={hamburger}
+          alt=""
+        />
+        <nav className="d-flex align-items-center">
+          <ul className="nav-list d-flex align-items-center">
+            <li className="nav-link">
+              <a href="">Browse</a>
+            </li>
+            <li className="nav-link" >
+              <a href="/setlists">
+                Setlists
+              </a>
+            </li>
+            <li className="nav-link" >
+              <button
+                onClick={() => Auth.logout()}
+                className="nav-log-out me-4"
+              >
+                Log Out
+              </button>
+            </li>
+            <img
+              onClick={toggleMenu}
+              className="plus-menu-exit"
+              src={plus}
+              alt="exit"
+            />
+          </ul>
         </nav>
       </div>
-    </div>
+    </header>
   );
 };

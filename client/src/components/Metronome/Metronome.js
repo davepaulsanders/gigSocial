e6a8af6103656feaf79ce80f67a390ab5576c947
Dave Sanders <davepaulsanders@gmail.com>
2022-07-25 15:17:26 -0400

    Metronome pulses

--- client/src/components/Metronome/Metronome.js
+++ client/src/components/Metronome/Metronome.js
@@ -1,14 +1,52 @@
-import React from "react";
-import "./Metronome.css"
-const upArrow = require('../../assets/up.png')
+import React, { useState } from "react";
+import "./Metronome.css";
+const upArrow = require("../../assets/up.png");
 export const Metronome = () => {
+  let [bpm, setBpm] = useState(120);
+
+  const toggleMetronome = (e) => {
+    e.preventDefault();
+    const metronome = document.querySelector(".metronome-circle");
+    if (metronome.classList.contains("pulse")) {
+      metronome.classList.remove("pulse");
+    } else {
+      metronome.classList.add("pulse");
+    }
+  };
+
+  const bpmTempoSet = (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains("up-arrow")) {
+      if (bpm === 350) {
+        return;
+      }
+      setBpm(++bpm);
+    }
+    if (e.target.classList.contains("down-arrow")) {
+      if (bpm === 40) {
+        return;
+      }
+      setBpm(--bpm);
+    }
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center">
-      <img className="down-arrow" src={upArrow} alt="increase metronome" />
-      <div className="metronome-circle mx-2">
-        <p className="metronome-bpm">120</p>
+      <img
+        onClick={bpmTempoSet}
+        className="down-arrow"
+        src={upArrow}
+        alt="increase metronome"
+      />
+      <div onClick={toggleMetronome} className="metronome-circle mx-2">
+        <p className="metronome-bpm">{bpm}</p>
       </div>
-      <img className="up-arrow" src={upArrow} alt="increase metronome" />
+      <img
+        onClick={bpmTempoSet}
+        className="up-arrow"
+        src={upArrow}
+        alt="increase metronome"
+      />
     </div>
   );
 };

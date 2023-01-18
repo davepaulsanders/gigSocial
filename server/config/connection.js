0f9bb90c17cbbe856ecd947e1049b929d4a22578
Dave Sanders <davepaulsanders@gmail.com>
2023-01-18 12:11:33 -0500

    dotenv for production

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 // options object no longer required, its default is what we want
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gig-social");
 module.exports = mongoose.connection;
